828b2b4ad56c8a1d536fe3dd44831713e59566a4
leonardomoretti11111-hash <leonardomoretti11111@gmail.com>
2025-10-04 01:14:23 +0200

    AppSoon.js

---
+++ src/components/AppSoon.js
@@ -0,0 +1,8 @@
+export default function AppSoon() {
+  return (
+    <div style={{ textAlign: "center", padding: "4rem" }}>
+      <h2>🚀 Full App Coming Soon</h2>
+      <p>The roar is just beginning…</p>
+    </div>
+  );
+}
